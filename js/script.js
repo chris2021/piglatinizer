4cbca9b7fb8dfe0c420517b355231cba30692362
chris2021 <cdavis2021@hshm.info>
2019-10-28 22:01:35 +0000

    finish

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
-$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-function pig(str)
-$("button").click(()
-    let message = $("input").val();
-    $("body").append(message);
-})
+$( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.)
 
 
-
-
+//test_array.forEach(function(items,index,my_array) {
+    //my_array[index]=items+"ay"
+//});
+//console.log(test_array);
+function adday(mystring){
+    return mystring + "ay";
+}
+$("button").click(() => {
+    let message= $("input").val();
+    //adday(message)
+    let newmessage= adday(message);
+  $("body").append(newmessage);
+});
 
 
 });
